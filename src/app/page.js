318764fdf3e52590d09b383fac676a4c6753ec69
qurbonov <qurbonov.p@gmail.com>
2024-05-28 15:40:21 +0500

    mainpage

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,38 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Personal web page
-    </main>
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    {/* IMAGE CONTAINER */}
+    <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+      <Image src="/me.png" alt="" fill className="object-contain" />
+    </div>
+    {/* TEXT CONTAINER */}
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center ml-4 md:mt-4">
+      {/* TITLE */}
+      <h1 className="text-4xl md:text-6xl font-bold">
+        Crafting Digital Experiences, Designing Tomorrow.
+      </h1>
+      {/* DESC */}
+      <p className="md:text-xl">
+        Welcome to my digital canvas, where innovation and creativity
+        converge. With a keen eye for aesthetics and a mastery of code, my
+        portfolio showcases a diverse collection of projects that reflect my
+        commitment to excellence.
+      </p>
+      {/* BUTTONS */}
+      <div className="w-full flex gap-4">
+        <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+          View My Work
+        </button>
+        <button className="p-4 rounded-lg ring-1 ring-black">
+          Contact Me
+        </button>
+      </div>
+    </div>
+  </div>
+    // <main className="flex flex-col items-center justify-between ">
+     
+    //    <Image src="/me.png" width={340} height={240} className="rounded-full p-5"/>
+    // </main>
   );
 }
---
+++ src/components/navLink.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({ link }) => {
+  const pathName = usePathname();
+  return (
+    // text-sky-900
+    <Link
+      className={` p-1 ${
+        pathName === link.url && "text-teal-400 border-teal-400 border-b-2"
+      }`}
+      href={link.url}
+    >
+      {link.title}
+    </Link>
+  );
+};
+
+export default NavLink;

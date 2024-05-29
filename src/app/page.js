4612d243368a851102a7bc4d39fbc21a17ed22a7
Qurbonov Pulatjon <Qurbonov.P@gmail.com>
2024-05-29 13:27:35 +0500

    page changed

--- src/app/page.js
+++ src/app/page.js
@@ -7,29 +7,6 @@ export default function Home() {
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
         <Image src="/me.png" alt="" fill className="object-contain" />
       </div>
-      {/* TEXT CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col sm:gap-0 items-center justify-center">
-        {/* TITLE */}
-        <h1 className="text-4xl md:text-6xl ">
-          Crafting Digital Experiences, Designing Tomorrow.
-        </h1>
-        {/* DESC */}
-        <p className="md:text-xl">
-          Welcome to my digital canvas, where innovation and creativity
-          converge. With a keen eye for aesthetics and a mastery of code, my
-          portfolio showcases a diverse collection of projects that reflect my
-          commitment to excellence.
-        </p>
-        {/* BUTTONS */}
-        <div className="w-full flex gap-4">
-          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
-            View My Work
-          </button>
-          <button className="p-4 rounded-lg ring-1 ring-black">
-            Contact Me
-          </button>
-        </div>
-      </div>
     </div>
     // <main className="flex flex-col items-center justify-between ">
 
